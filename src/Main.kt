2ce9f144838752720af36f4f79c4d46a2d632d4e
zintinux27 <rebinsor1300estudio@gmail.com>
2024-10-30 12:30:21 +0100

    3rd commit

--- src/Main.kt
+++ src/Main.kt
@@ -4,6 +4,7 @@ fun main() {
     println("Project to show how to work Git with IntelliJ")
     helloWorld("Rumewert name")
 }
+//3rd commit prob
 
 fun helloWorld(name: String){
     println("Hello $name")
